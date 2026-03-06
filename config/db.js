48a13f94b17ec73eef7e6a83d29c4650f6cbe780
Rajnish921 <rajnishkumar18at@gmail.com>
2026-03-06 21:58:17 +0530

    Added SSL config for Railway MySQL

--- config/db.js
+++ config/db.js
@@ -1,11 +1,14 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
-
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 async function testConnection() {
@@ -20,4 +23,4 @@ async function testConnection() {
 
 testConnection();
 
-module.exports = pool;
+((module.exports = pool), promise());
